bbf3f284e5650d2270a152fe46c994c661570a2c
koopa1338 <sinner1991@gmail.com>
2023-04-06 12:27:16 +0200

    switching to leptos for frontend crate

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,16 @@
-#![recursion_limit = "1024"]
+use leptos::*;
+use leptos_meta::*;
 
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-use console_error_panic_hook::set_once as set_panic_hook;
-use yew::prelude::*;
-
-struct App;
-
-impl Component for App {
-    type Message = ();
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self
-    }
-
-    fn update(&mut self, _ctx: &Context<Self>, _msg: Self::Message) -> bool {
-        false
-    }
-
-    fn view(&self, _ctx: &Context<Self>) -> Html {
-        html! {
-            <div class="p-10 bg-slate-500">
-                <h1 class="text-gray-200 font-bold text-3xl">{"Hello World"}</h1>
-            </div>
-        }
+#[component]
+pub fn Root(cx: Scope) -> impl IntoView {
+    provide_meta_context(cx);
+    view! { cx,
+        "TODO"
     }
 }
 
 fn main() {
-    set_panic_hook();
-
-    yew::start_app::<App>();
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+    mount_to_body(|cx| view! { cx,  <Root/> })
 }
